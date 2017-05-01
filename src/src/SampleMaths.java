e53eee2bfd73e1722fc95e56c64c5742e3279074
barovig <semargl1349@gmail.com>
2017-05-01 11:26:26 +0100

    Cleaned up the code

--- src/src/SampleMaths.java
+++ src/src/SampleMaths.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package src;
 
-/**
- *
- * @author semargl
- */
 public class SampleMaths {
     
     int ia;
@@ -17,39 +8,30 @@ public class SampleMaths {
     double db;
     
     public <T extends Number> double add(T a, T b, Class c){
-        
         if(c == Double.class){
             double n1 = a.doubleValue();
             double n2 = b.doubleValue();
             return n1 + n2; 
         }
-        
         if(c == Integer.class){
             int n1 = a.intValue();
             int n2 = b.intValue();
             return n1 + n2;             
         }
-        
         return Double.NaN;
     }
-    
-        public <T extends Number> double multiply(T a, T b, Class c){
+    public <T extends Number> double multiply(T a, T b, Class c){
         
         if(c == Double.class){
             double n1 = a.doubleValue();
             double n2 = b.doubleValue();
             return n1 * n2; 
         }
-        
         if(c == Integer.class){
             int n1 = a.intValue();
             int n2 = b.intValue();
             return n1 * n2;             
         }
-        
         return Double.NaN;
-    }
-    
-    
-    
+    }    
 }
